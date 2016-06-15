c9019bb74ce85580e15143234a0e4b735ffc2080
jmverrelli <jmverrelli@localhost.localdomain>
2016-06-15 15:01:03 -0300

    Limpio a ver si encuentro que esta sobreescrbiendo session...

--- forms/modiUsuario.php
+++ forms/modiUsuario.php
@@ -9,9 +9,9 @@ include_once '../dbLinker/user.class.php';
 
 <form id="formModiUsuario" name="formModiUsuario" class="formInsumo">
 
-<div class="conLabel"><label class="etiqueta" for="usuario">Usuario</label><input type="text" name="usuario" value=<?php echo "'".unserialize($user->getUsuario()."'"; ?> disabled="disabled"/></div>
+<div class="conLabel"><label class="etiqueta" for="usuario">Usuario</label><input type="text" name="usuario" value=<?php echo "'".unserialize($usuario->getUsuario()."'"; ?> disabled="disabled"/></div>
 
 <div class="conLabel"><label class="etiqueta" for="password">Password</label><input type="password" name="password" /></div>
-<input type="password" name="usuario" style="display:none; visibility:hidden;" value=<?php echo "'".$user->getUsuario()."'"; ?> /></div>
-<input type="password" name="id" style="display:none; visibility:hidden;" value=<?php echo "'".$user->getId()."'"; ?> /></div>
+<input type="password" name="usuario" style="display:none; visibility:hidden;" value=<?php echo "'".$usuario->getUsuario()."'"; ?> /></div>
+<input type="password" name="id" style="display:none; visibility:hidden;" value=<?php echo "'".$usuario->getId()."'"; ?> /></div>
 </form>
