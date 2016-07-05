88580ddc4f4b837430588c92c7db2e758ac4835c
jmverrelli <jmverrelli@localhost.localdomain>
2016-07-05 12:03:11 -0300

    La modificacion de usuario no estaba hecha para la modificacion de clase que tenia, lo arregle

--- forms/modiUsuario.php
+++ forms/modiUsuario.php
@@ -8,14 +8,16 @@ if(!isset($_SESSION['usuarioSoporteInf']))
   exit;
 }
 
-include_once '../dbLinker/user.class.php';
+include_once '../dbLinker/informaticaDatabaseLinker.class.php';
+$infDb = new informaticaDataBaseLinker();
  $usuario = $infDb->devolverUsuario($_SESSION['usuarioSoporteInf']);
+ $user = $usuario->getUsuario();
 
 ?>
 
 <form id="formModiUsuario" name="formModiUsuario" class="formInsumo">
 
-<div class="conLabel"><label class="etiqueta" for="usuario">Usuario</label><input type="text" name="usuario" value=<?php echo "'".unserialize($usuario->getUsuario()."'"; ?> disabled="disabled"/></div>
+<div class="conLabel"><label class="etiqueta" for="usuario">Usuario</label><input type="text" name="usuario" value=<?php echo "'".$user."'"; ?> disabled="disabled"/></div>
 
 <div class="conLabel"><label class="etiqueta" for="password">Password</label><input type="password" name="password" /></div>
 <input type="password" name="usuario" style="display:none; visibility:hidden;" value=<?php echo "'".$usuario->getUsuario()."'"; ?> /></div>
